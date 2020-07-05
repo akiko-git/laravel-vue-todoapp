24da00b0d3be29f1278b843cb1848099ef87f5f5
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-07-06 01:32:35 +0900

    Vueの使用設定

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,9 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+Vue.use(Vuetify);
 
 /**
  * The following block of code may be used to automatically register your
