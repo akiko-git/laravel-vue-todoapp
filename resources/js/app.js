e83f497901ee7849ce5f228270808e36805240ce
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-07-27 02:26:33 +0900

    エラー解消

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,8 +9,8 @@ require('./bootstrap');
 window.Vue = require('vue');
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+import '@mdi/font/css/materialdesignicons.css';
 import axios from "axios";
 Vue.use(Vuetify);
 
@@ -36,4 +36,5 @@ Vue.component('task', require('./components/TaskComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
+		vuetify : new Vuetify(),
 });
