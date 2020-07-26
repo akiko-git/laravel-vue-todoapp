485a7f66101b5f03c001bdd895b0a826334518b3
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-07-27 02:24:44 +0900

    Date picker追加、エラー解消

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -1,51 +1,72 @@
 <template>
-
-
-<v-card
-	max-width="1000"
-	class="mx-auto"
->
+<v-app>
 	<v-container>
-
+		<h1>Todolist</h1>
 			<v-form @submit.prevent="add()">
 			<v-row>
-				<v-col class="mx-auto">
-					<v-text-field v-model="addTask" name="addTask" label="add task" solo>
-						<template v-slot:append-outer>
-							<v-btn type="submit" class="mx-2" fab dark color="#3F51B5">
-								<v-icon color="#FFFFFF">mdi-plus</v-icon>
-							</v-btn>	
+				<v-col class="mx-auto" cols="8">
+					<v-text-field v-model="addTaskTitle" name="addTaskTitle" label="add task" solo></v-text-field>
+					<v-text-field v-model="addTaskText" name="addTaskText" label="add task text" solo></v-text-field>
+					<v-menu
+					 ref="menu"
+					 v-model="menu"
+					 :close-on-content-click="false"
+					 :return-value.sync="date"
+					 transition="scale-transition"
+					 offset-y
+					 min-width="290px"
+					>
+						<template v-slot:activator="{ on,attrs }">
+							<v-text-field
+							 v-model="date"
+							 label="期限"
+							 prepend-icon="event"
+							 readonly
+							 v-bind="attrs"
+							 v-on="on"
+						></v-text-field>
 						</template>
-					</v-text-field>
+						<v-date-picker v-model="date" no-title scrollable>
+          		<v-spacer></v-spacer>
+          		<v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+          		<v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+        		</v-date-picker>
+					</v-menu>
+					<v-btn type="submit" class="mx-2">
+						送信
+					</v-btn>	
 				</v-col>
 			</v-row>
 		</v-form>
 
 		<v-row dense>
 			<v-col class="mx-auto" cols="8" v-for="list in lists">
-				<v-card
-					color="#E8EAF6"
-				>
-					<v-card-title class="headline">
-						<v-card-title>{{ list.title }}</v-card-title>
-						<v-card-text>{{ list.text }}</v-card-text>
-						<template>
-							<v-icon @click="deleteTask(list.id)">mdi-trash-can</v-icon>
-						</template>	
-					</v-card-title>
-				</v-card>
+					<v-list two-line color="#E8EAF6" max-width="500" class="mx-auto">
+						<v-list-item-group>
+							<v-list-item>
+								<v-list-item-content>
+									<v-list-item-title>{{ list.title }}</v-list-item-title>
+									<v-list-item-subtitle>{{ list.text }}</v-list-item-subtitle>
+								</v-list-item-content>
+								<v-icon @click="deleteTask(list.id)">mdi-trash-can</v-icon>
+							</v-list-item>	
+						</v-list-item-group>
+					</v-list>
 			</v-col>
 		</v-row>
 	</v-container>
-</v-card>
+</v-app>
 </template>
 
 <script>
     export default {
 			data(){
 				return{
-					addTask:"",
+					addTaskTitle:"",
+					addTaskText:"",
 					lists:[],
+					date: new Date().toISOString().substr(0, 10),
+					menu: false,
 				};
 			},
 			created(){
@@ -53,11 +74,16 @@
 					this.lists = res.data.getlist;
 					return true;
 				});
+				this.$vuetify.lang = {
+        	t : () => {},
+      	}
+      	this.$vuetify.theme = { dark : false}
 			},
 			methods:{
 				add:function(){
 					const dataform = new FormData();
-					dataform.append('addTask',this.addTask);
+					dataform.append('addTaskTitle',this.addTaskTitle);
+					dataform.append('addTaskText',this.addTaskText);
 					axios.post('http://localhost:8001/todolist/form',dataform).then(res => {
 						console.log(res.data.success);
 						this.lists.push(res.data.success);
