347443824cca514337e466f26eb3a51f404820f8
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-10-07 18:25:55 +0900

    ダイアログ機能追加

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,81 +1,77 @@
 <template>
-	<div>
-			<v-navigation-drawer
-				v-model="drawer"
-				app
-				clipped
-			>
-				<v-list>
-					<v-list-item>
-						<v-list-item-icon>
-							<v-icon>mdi-home</v-icon>
-						</v-list-item-icon>
-						<v-list-item-title>Home</v-list-item-title>
-					</v-list-item>	
-					<v-list-group @mouseover="mouseOverAction" @mouseleave="mouseLeaveAction">
-						<template v-slot:activator>
-							<v-list-item-content>
-								<v-list-item-title class="iconPotision">プロジェクト</v-list-item-title>
-								<!--<i v-if="hoverFlag" class="fas fa-plus"></i>-->
-								<i v-if="hoverFlag" class="fas fa-plus" @click="addProject"></i>
-							</v-list-item-content>
-						</template>
-						<v-list-item
-						 v-for="(pro,i) in pros"
-						 :key="i"
-						 link
-						>
-							<v-list-item-title v-text="pro[0]"></v-list-item-title>
-						</v-list-item>
-					</v-list-group>
-				</v-list>
-			</v-navigation-drawer>
-			<v-app-bar
-			  class="indigo"
-				fixed
-				app
-				dark
-				clipped-left
-			>
-				<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-				<v-toolbar-title>Title</v-toolbar-title>				
-			</v-app-bar>
-
-		</div>	
+  <div>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+        <v-list-group
+          @mouseover="mouseOverAction"
+          @mouseleave="mouseLeaveAction"
+          v-bind:disabled="isPush"
+        >
+          <template v-slot:activator>
+            <ProjectDialog @close="isPush = false">
+              <template v-slot:activator="{ on }">
+                <!-- <i v-if="hoverFlag" class="fas fa-plus" @click="on"></i> -->
+                <v-icon v-on="on" small @click="pushIcon()">fas fa-plus</v-icon>
+              </template>
+            </ProjectDialog>
+            <v-list-item-content>
+              <v-list-item-title>プロジェクト</v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item v-for="(pro, i) in pros" :key="i" link>
+            <v-list-item-title v-text="pro[0]"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar class="indigo" fixed app dark clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
-	export default {
-		data:()=> ({
-			drawer:null,
-			pros:[
-				['お知らせ'],
-			],
-			hoverFlag: false,
-		}),
-		methods:{
-			mouseOverAction(){
-        this.hoverFlag = true
-      },
-      mouseLeaveAction(){
-        this.hoverFlag = false
-      },
-			addProject(){
-				alert("add!!!")
-			}
-		},
-
-	}
-
+export default {
+  data: () => ({
+    drawer: null,
+    pros: [["お知らせ"]],
+    hoverFlag: false,
+    isPush: false,
+  }),
+  methods: {
+    mouseOverAction() {
+      this.hoverFlag = true;
+    },
+    mouseLeaveAction() {
+      this.hoverFlag = false;
+    },
+    addProject() {
+      alert("add!!!");
+    },
+    pushIcon() {
+      this.isPush = true;
+    },
+    test() {
+      alert("test");
+    },
+  },
+  components: {
+    ProjectDialog: () => import("./ProjectDialog"),
+  },
+};
 </script>
 <style>
-	.iconPotision{
- 		position: relative;
-    margin-left: 20px;
-	}
-	.fa-plus {
-		position: absolute;
-		top: 15px;
-	}
-
+.v-list-item__content {
+  padding-left: 20px;
+}
+.v-icon.v-icon.v-icon--link {
+  padding-left: 5px;
+}
 </style>
---
+++ resources/js/components/ProjectDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <slot name="activator" :on="{ click: open }"></slot>
+    </template>
+    <v-card>
+      <v-card-title>
+        <h1>入力フォーム</h1>
+      </v-card-title>
+      <v-card-text>
+        <form>
+          <v-text-field v-model="text" :counter="10"></v-text-field>
+        </form>
+      </v-card-text>
+      <v-card-actions>
+        <v-space></v-space>
+        <v-btn color="blue darken-1" text @click="cancel">キャンセル</v-btn>
+        <v-btn color="blue darken-1" text @click="dialog = false">追加</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    text: "",
+  }),
+  methods: {
+    open() {
+      this.dialog = true;
+    },
+    cancel() {
+      this.dialog = false;
+      this.$emit("close");
+    },
+  },
+};
+</script>
