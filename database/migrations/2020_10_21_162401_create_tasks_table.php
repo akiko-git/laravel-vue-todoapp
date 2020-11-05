8a03b33bb8615ce8e5b4488419f5b0151e480931
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-11-05 17:00:44 +0900

    カラムの変更

---
+++ database/migrations/2020_10_21_162401_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('text')->nullable();
+            $table->date('deadline')->nullable();
+            $table->text('status')->nullable();
+            $table->foreignId('project_id')->nullable()->constrained()->onDelete('cascade');
+						//$table->foreignId('category_id')->constrained('categories_table');
+						//$table->foreignId('users_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
