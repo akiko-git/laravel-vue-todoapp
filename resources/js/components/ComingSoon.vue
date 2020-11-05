a8e1bf2360a9f11aa091ee59a46f2dd8a46d3237
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-11-05 17:02:21 +0900

    編集、新規登録機能実装

--- resources/js/components/ComingSoon.vue
+++ resources/js/components/ComingSoon.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>近日予定です！！</div>
-</template>
+  <v-row class="fill-height">
+    <TaskDialog
+      ref="taskAddDialog"
+      dialogTitle="新規登録"
+      dialogBtnText="追加"
+    ></TaskDialog>
+    <v-col>
+      <v-toolbar flat>
+        <v-btn fab text small color="grey darken-2" @click="prev">
+          <v-icon small> mdi-chevron-left </v-icon>
+        </v-btn>
+        <v-toolbar-title v-if="$refs.calendar">
+          {{ $refs.calendar.title }}
+        </v-toolbar-title>
+        <v-btn fab text small color="grey darken-2" @click="next">
+          <v-icon small> mdi-chevron-right </v-icon>
+        </v-btn>
+        <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+          Today
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-menu bottom right>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+              <span>{{ typeToLabel[type] }}</span>
+              <v-icon right> mdi-menu-down </v-icon>
+            </v-btn>
+          </template>
           <v-list>
             <v-list-item @click="type = 'day'">
               <v-list-item-title>Day</v-list-item-title>
@@ -25,26 +51,164 @@
           @click:event="showEvent"
           @click:date="viewDay"
           @change="updateRange"
+          @click:day="openTaskDialogAsAdd"
         ></v-calendar>
         <v-menu
           v-model="selectedOpen"
           :close-on-content-click="false"
+          :close-on-click="false"
           :activator="selectedElement"
           offset-x
         >
           <v-card color="grey lighten-4" min-width="350px" flat>
-            <v-card-text>
-              <span v-html="selectedEvent.name"></span> </v-card-text
-          ></v-card>
+            <v-toolbar color="grey lighten-4" flat>
+              <v-toolbar-title>{{ selectedEvent.name }}</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon @click="openTaskDialogAsEdit">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-trash-can</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-actions>
+              <v-btn text color="blue darken-1" @click="selectedOpen = false">
+                キャンセル
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-menu>
+        <TaskDialog
+          ref="taskEditDialog"
+          dialogTitle="編集"
+          dialogBtnText="保存"
+          :eventObj="selectedEvent"
+          @close="editDialogClose"
+        ></TaskDialog>
       </v-sheet>
     </v-col>
   </v-row>
 </template>
 <script>
+import TaskDialog from "./TaskDialog";
 export default {
-  created() {
-    console.log("近日公開だよ");
+  components: {
+    TaskDialog,
+  },
+
+  data: () => ({
+    focus: "",
+    type: "month",
+    typeToLabel: {
+      month: "Month",
+      week: "Week",
+      day: "Day",
+    },
+    selectedEvent: {},
+    selectedElement: null,
+    selectedOpen: false,
+    events: [],
+    colors: ["blue", "indigo", "cyan", "green", "pink", "orange"],
+  }),
+  methods: {
+    viewDay({ date }) {
+      this.type = "day";
+      this.focus = date;
+      console.log("viewDay");
+      console.log(this.focus);
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        setTimeout(() => {
+          this.selectedOpen = true;
+        }, 10);
+      };
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+      nativeEvent.stopPropagation();
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    setToday() {
+      console.log(this.focus);
+      this.focus = "";
+    },
+    updateRange(E) {
+      // console.log("updateRange");
+      // console.log(E);
+      // console.log(this.rnd(0, 3));
+    },
+    //タスクの追加
+    openTaskDialogAsAdd({ date }) {
+      // console.log(e);
+      // this.selectedEvent = "";
+      // this.editEvent = this.selectedEvent;
+      this.$refs.taskAddDialog.open(date);
+    },
+    //タスクの編集
+    openTaskDialogAsEdit() {
+      // this.editEvent = this.selectedEvent;
+      this.$refs.taskEditDialog.open(this.selectedEvent.start);
+    },
+    //タスクの編集画面を閉じた時の処理
+    editDialogClose() {
+      this.selectedOpen = false;
+    },
+    // updateRange({ start, end }) {
+    //   const min = new Date(`${start.date}T00:00:00`);
+    //   const max = new Date(`${end.date}T23:59:59`);
+    //   const days = (max.getTime() - min.getTime()) / 86400000;
+    //   const eventCount = this.rnd(days, days + 20);
+    //   console.log(eventCount);
+    // },
+    rnd(min, max) {
+      return Math.floor((max - min + 1) * Math.random()) + min;
+    },
+  },
+  props: {
+    taskList: {
+      type: Array,
+      default: null,
+    },
+  },
+  computed: {
+    pushEvents: function () {
+      this.events = taskList;
+    },
+  },
+  created() {},
+  mounted() {
+    let currentProjectId = null;
+    for (let i in this.taskList) {
+      // if (this.taskList[i].project_id == null) {
+      //   this.currentProjectId = "null";
+      // } else {
+      //   this.currentProjectId = this.taskList[i].project_id;
+      // }
+      this.events.push({
+        name: this.taskList[i].title,
+        start: this.taskList[i].deadline,
+        end: this.taskList[i].deadline,
+        color: this.colors[this.rnd(0, this.colors.length - 1)],
+        taskId: this.taskList[i].id,
+        projectId: this.taskList[i].project_id,
+      });
+    }
+    // console.log(this.$refs.calendar);
+    // console.log(this.events);
   },
 };
 </script>
