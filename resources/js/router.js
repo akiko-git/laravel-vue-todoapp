060a6bc74805d4f8ba030e59e35e161e7e82f63c
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-12-02 17:12:25 +0900

    metaタグ追加、書き方修正

--- resources/js/router.js
+++ resources/js/router.js
@@ -2,17 +2,30 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import TaskComponent from './components/TaskComponent.vue';
-
+// import NotFoundComponent from './components/NotFoundComponent.vue';
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     routes: [
         {
             path: '/',
             name: 'task',
             component: TaskComponent,
+            meta: { title: 'タスク一覧' },
         },
+        // {
+        //     path: '*',
+        //     component: NotFoundComponent,
+        // }
     ],
 })
+
+router.afterEach((to, from) => {
+    if (to.meta && to.meta.title) {
+        document.title = to.meta.title
+    }
+})
+
+export default router;
