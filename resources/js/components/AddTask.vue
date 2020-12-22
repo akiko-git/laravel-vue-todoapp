236eb86f2488c32b4227d7b4c9150e1de0fa444e
akiko-git <xxx.1.blaze.aki@gmail.com>
2020-12-22 19:54:39 +0900

    レイアウト変更、リファクタリング

--- resources/js/components/AddTask.vue
+++ resources/js/components/AddTask.vue
@@ -1,59 +1,114 @@
 <template>
   <div class="addTask">
-    <v-list v-if="isActive">
+    <v-list v-if="isActive" max-width="790">
       <v-list-item small @click="active" class="textColor">
         <v-list-item-icon class="mr-2">
           <v-icon small>fas fa-plus</v-icon>
         </v-list-item-icon>
-        <v-list-item-title>プロジェクトを追加</v-list-item-title>
+        <v-list-item-title>タスクを追加</v-list-item-title>
       </v-list-item>
     </v-list>
-    <v-form @submit.prevent="add()" v-if="!isActive" class="addTask_form">
-      <v-row>
-        <v-col class="mx-auto mb-8" cols="8">
+    <v-form
+      @submit.prevent="add()"
+      v-if="!isActive"
+      class="addTask_form"
+      v-click-outside="{
+        handler: close,
+        closeConditional,
+      }"
+    >
+      <v-row justify="center" align-content="center" dense>
+        <v-col cols="11" class="mt-2">
           <v-text-field
             v-model="task.title"
-            label="title"
-            outlined
+            label="タスクを入力して下さい"
+            solo
+            single-line
           ></v-text-field>
-          <v-textarea
-            v-model="task.text"
-            label="text"
-            outlined
-            auto-grow
-          ></v-textarea>
+        </v-col>
+        <v-col cols="11" class="mb-4">
+          <!-- 日付 -->
           <v-menu
             ref="menu"
             v-model="menu"
             :close-on-content-click="false"
-            :return-value.sync="task.date"
+            :return-value.sync="task.deadline"
             transition="scale-transition"
             offset-y
             min-width="290px"
           >
             <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                v-model="task.date"
+              <!-- <v-text-field
+                v-model="task.deadline"
                 label="期限"
                 prepend-inner-icon="event"
                 readonly
                 v-bind="attrs"
                 v-on="on"
-              ></v-text-field>
+              ></v-text-field> -->
+              <v-chip
+                class="mr-2"
+                color="blue"
+                outlined
+                v-bind="attrs"
+                v-on="on"
+                v-model="task.deadline"
+              >
+                {{ task.deadline }}
+              </v-chip>
             </template>
-            <v-date-picker v-model="task.date" no-title scrollable>
+            <v-date-picker v-model="task.deadline" no-title scrollable>
               <v-spacer></v-spacer>
               <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-              <v-btn text color="primary" @click="$refs.menu.save(task.date)"
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(task.deadline)"
                 >OK</v-btn
               >
             </v-date-picker>
           </v-menu>
-          <v-row>
-            <v-btn type="submit" class="mx-2" color="primary"> 送信 </v-btn>
-            <v-btn outlined color="primary" @click="close">キャンセル</v-btn>
-          </v-row>
-          <div>{{ projectId }}<br />{{ deadline }}<br />{{ category }}</div>
+          <!-- プロジェクトの種類 -->
+          <v-menu v-model="projectMenu" :nudge-width="200" offset-x>
+            <template v-slot:activator="{ on, attrs }">
+              <v-chip
+                class="mr-2"
+                color="green"
+                outlined
+                v-bind="attrs"
+                v-on="on"
+                v-model="task.project_id"
+              >
+                {{ projectName }}
+              </v-chip>
+            </template>
+            <v-card>
+              <v-list>
+                <v-list-item link>
+                  <v-list-item-title @click="handleSaveProjectId(null)">
+                    インボックス
+                  </v-list-item-title>
+                </v-list-item>
+                <v-list-item link v-for="(list, index) in this.getProjects">
+                  <v-list-item-title @click="handleSaveProjectId(list.id)">
+                    {{ list.project }}
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-menu>
+        </v-col>
+        <v-col cols="11">
+          <v-btn
+            :disabled="activeSave"
+            type="submit"
+            class="mx-2"
+            color="primary"
+          >
+            送信
+          </v-btn>
+          <v-btn outlined color="primary" @click="close">キャンセル</v-btn>
+          <div>{{ projectId }}<br />{{ type }}</div>
         </v-col>
       </v-row>
     </v-form>
@@ -65,50 +120,68 @@ export default {
   data() {
     return {
       isActive: true,
-      addTaskTitle: "",
-      addTaskText: "",
-      lists: [],
-      date: new Date().toISOString().substr(0, 10),
+      deadline: new Date().toISOString().substr(0, 10),
       menu: false,
-      task: {
-        title: "",
-        text: "",
-        date: new Date().toISOString().substr(0, 10),
-        project_id: this.projectId,
-      },
+      task: {},
+      projectMenu: false,
+      projectName: "",
     };
   },
   methods: {
     ...mapActions("task", ["fetchTasks", "creatTask"]),
 
     active() {
-      this.isActive = !this.isActive;
-      this.task["title"] = "";
-      this.task["text"] = "";
+      this.isActive = false;
+      this.task.deadline = new Date().toISOString().substr(0, 10);
+      this.task.status = 1;
+      // this.task["title"] = "";
+      if (this.type === "project") {
+        // this.task.inbox_flag = false;
+        if (this.projectId) {
+          this.task.project_id = this.projectId;
+          const getProject = this.getProjects.find(
+            ({ id }) => id === this.projectId
+          );
+          this.projectName = getProject.project;
+        }
+      } else {
+        // this.task.inbox_flag = true;
+        this.projectName = "インボックス";
+        this.task.project_id = null;
+      }
     },
     close() {
-      this.isActive = !this.isActive;
+      this.task = {};
+      this.isActive = true;
+    },
+    closeConditional(e) {
+      if (this.menu === false && this.projectMenu === false) {
+        return true;
+      } else {
+        return false;
+      }
     },
     add() {
       this.creatTask(this.task).then((res) => {
+        console.log("追加したタスク");
+        console.log(this.task);
         if (res === true) {
           alert("タスクを追加しました");
         } else {
           alert("タスクの追加に失敗しました");
         }
       });
-      this.isActive = !this.isActive;
-      // this.close();
-      // axios
-      //   .post("http://localhost:8001/api/todolist/form", this.task)
-      //   .then((res) => {
-      //     console.log(res.data.success);
-      //     this.lists.push(res.data.success);
-      //     this.$emit("success", res.data.success);
-      //     this.close();
-      //   });
-
-      // this.$emit("success", res.data.success);
+      this.isActive = true;
+    },
+    handleSaveProjectId(setId) {
+      if (setId) {
+        this.task.project_id = setId;
+        const setProject = this.getProjects.find(({ id }) => id === setId);
+        this.projectName = setProject.project;
+      } else {
+        this.task.project_id = null;
+        this.projectName = "インボックス";
+      }
     },
   },
   props: {
@@ -116,19 +189,27 @@ export default {
       type: [Number, String],
       default: "",
     },
-    deadline: {
-      type: String,
-      default: "",
-    },
-    category: {
+    type: {
       type: String,
       default: "",
     },
   },
+  computed: {
+    ...mapGetters("project", ["getProjects"]),
+    activeSave() {
+      if (this.task.title) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+  mounted() {},
 };
 </script>
 <style lang="scss" scoped>
 .addTask {
+  // width: 50%;
   margin-top: 20px;
   .isClick {
     display: block;
@@ -138,6 +219,7 @@ export default {
   }
   &_form {
     border: solid #000000 1px;
+    width: 100%;
   }
 }
 </style>
