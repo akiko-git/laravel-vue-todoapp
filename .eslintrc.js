51943dff910ce616f82467313db93b456e4c68cd
akiko-git <xxx.1.blaze.aki@gmail.com>
2021-01-21 16:11:41 +0900

    ESLint,Prettierのインストール・設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,32 @@
+'use strict';
+
 module.exports = {
+  root: true,
   extends: [
-    //'eslint:recommended',
-    'plugin:vue/recommended'
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
   ],
+  globals: {
+    axios: true,
+  },
   parserOptions: {
     parser: 'babel-eslint',
-    ecmaVersion: 2015
+    ecmaVersion: 2015,
   },
   env: {
     browser: true,
+    node: true,
   },
-  plugins: [
-    'vue'
-  ],
-}
+  plugins: ['vue'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+};
